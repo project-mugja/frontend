b902b389b64f90e723788ab6958ee06342ea7130
sunniesfish <hhch5611.jh@gmail.com>
2024-05-20 19:29:55 +0900

    일단 커밋

---
+++ src/components/Review.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const ReviewBox = styled.div`
+    border: none;
+    border-radius: 30px;
+    max-height: 324px;
+    width: 100%;
+`
+
+function Review(){
+    return(
+        <>
+        </>
+    )
+}
