b6f5aa472dc317518d194b2cf3669698a9b4b4f2
sunniesfish <hhch5611.jh@gmail.com>
2024-05-23 19:35:09 +0900

    room

---
+++ src/components/Room.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: grid;
+
+`
+const Img = styled.div`
+    
+`
+
+function Room(){
+    return(
+        <>
+            <Container>
+                <Img/>
+            </Container>
+        </>
+    )
+}
+
+export default Room;
--- src/routes/detail-page.tsx
+++ src/routes/detail-page.tsx
@@ -1,12 +1,22 @@
+import styled from "styled-components";
 import Hotel from "../components/Hotel";
 import Review from "../components/Review";
 import { Container } from "../theme";
+
+const RoomWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
 function DetailPage(){
     return(
         <>
             <Container>
                 <Hotel/>
+                <RoomWrapper>
+                    {}
+                </RoomWrapper>
                 <Review/>
             </Container>
         </>
