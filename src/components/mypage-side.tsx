8b64f929a024c91fe3d11f9e415905d0f8df5365
sunniesfish <hhch5611.jh@gmail.com>
2024-05-24 13:44:15 +0900

    뭐지?

--- src/components/mypage-side.tsx
+++ src/components/mypage-side.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components"
 import { IMyPageSideBarProps } from "../interface"
+import { useState } from "react"
+import { useQuery } from "react-query"
+import { useRecoilValue } from "recoil"
+import { isFavPage } from "../atom"
 
 const Wrapper = styled.div`
     display: flex;
@@ -15,8 +19,14 @@ const FavOptions = styled.div`
     flex-direction: column;
     width: 100%;
 `
-const FavOption = styled.div`
-    
+const MpTitle = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const MpOption = styled.div`
+    width: 100%;
 `
 interface IFT{
     isToggled:boolean;
@@ -30,8 +40,23 @@ const FavToggle = ({isToggled}:IFT) => {
     return(<Circle></Circle>)
 }
 
-function MyPageSideBar({isFavPage}:IMyPageSideBarProps){
-    const onToggle = () => {
+function MyPageSideBar(){
+    const isFavPage = useRecoilValue(isFavPage);
+    //찜목록
+    //useQuery로 카테고리 검색 실행하는 로직 필요
+    const [favCat, setFavCat] = useState(1);
+    const onToggle = (index:number) => {
+        setFavCat(index);
+    }
+
+    //마이페이지
+    const onMod = () => {
+        
+    }
+    const onBook = () => {
+
+    }
+    const onFav = () => {
 
     }
     return(
@@ -41,13 +66,35 @@ function MyPageSideBar({isFavPage}:IMyPageSideBarProps){
                 <FavTitle>필터</FavTitle>
                 <FavTitle>숙소 유형</FavTitle>
                 <FavOptions>
-                    <FavOption>
-                        <FavToggle isToggled={true}/><span> 전체 </span>
-                    </FavOption>
+                    <div onClick={()=>onToggle(1)}>
+                        <FavToggle isToggled={1 === favCat}/><span> 전체 </span>
+                    </div>
+                    <div onClick={()=>onToggle(2)}>
+                        <FavToggle isToggled={2 === favCat}/><span> 모텔 </span>
+                    </div>
+                    <div onClick={()=>onToggle(3)}>
+                        <FavToggle isToggled={3 === favCat}/><span> 호텔·리조트 </span>
+                    </div>
+                    <div onClick={()=>onToggle(4)}>
+                        <FavToggle isToggled={4 === favCat}/><span> 펜션 </span>
+                    </div>
+                    <div onClick={()=>onToggle(5)}>
+                        <FavToggle isToggled={5 === favCat}/><span> 홈&빌라 </span>
+                    </div>
                 </FavOptions>
              </> 
              : 
              <>
+                <MpTitle>마이페이지</MpTitle>
+                <MpOption onClick={onMod}>
+
+                </MpOption>
+                <MpOption onClick={onBook}>
+
+                </MpOption>
+                <MpOption onClick={onFav}>
+                    찜 목록
+                </MpOption>
              </>
             }
         </Wrapper>
