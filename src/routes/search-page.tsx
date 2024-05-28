84296fbf32ad1c7a6347d1a6085fe2868dff74bd
sunniesfish <hhch5611.jh@gmail.com>
2024-05-28 21:52:50 +0900

    navigate 왜 날라갔었지..

---
+++ src/routes/search-page.tsx
@@ -0,0 +1,16 @@
+import { ListPage, ListWrapper, SideBarWrapper } from "../components/components";
+
+function SearchPage(){
+    return(
+        <ListPage className="listPage">
+            <SideBarWrapper>
+
+            </SideBarWrapper>
+            <ListWrapper className="listWrapper">
+     
+            </ListWrapper>
+        </ListPage>
+    )
+}
+
+export default SearchPage;
