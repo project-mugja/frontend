4532ed20991dfc3a256d19aa314e35ee00cd40e4
sunniesfish <hhch5611.jh@gmail.com>
2024-06-03 19:31:33 +0900

    수정

--- src/components/Hotel.tsx
+++ src/components/Hotel.tsx
@@ -110,7 +110,7 @@ function Hotel({data, reviews, token}:IHostProp){
     const [isMap, setIsMap] = useState(false);
     const [isLiked, setIsLiked] = useState(false)
     const {data:isFav} = useQuery(["fav",data.hostId, isLiked],() => isFavFn(data.hostId,token),{
-        onSuccess:(data:boolean) => setIsLiked(data),
+        onSuccess:(data:boolean) => {setIsLiked(data); console.log("isLiked ",data)},
     })
     useEffect(()=>{
         const script = document.createElement('script');
@@ -125,7 +125,7 @@ function Hotel({data, reviews, token}:IHostProp){
         }
     },[])
     const onFavClick = () => {
-        isFav? 
+        isLiked? 
             delFav(data.hostId,token).then(()=>setIsLiked(false)) 
             : 
             addFav(data.hostId,token).then(()=>setIsLiked(true));
@@ -148,7 +148,7 @@ function Hotel({data, reviews, token}:IHostProp){
                             onClick={onFavClick} 
                             xmlns="http://www.w3.org/2000/svg" 
                             viewBox="0 0 512 512"
-                            fill={isFav? "#ff4752" : "gray"}
+                            fill={isLiked? "#ff4752" : "gray"}
                         >
                                 <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
                         </svg> 
