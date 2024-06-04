c97fcf4f43c7b2d1149d34bf77239b504d1d53be
sunniesfish <hhch5611.jh@gmail.com>
2024-06-05 01:33:10 +0900

    searchbox

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -42,7 +42,25 @@ const Title = styled.div`
     }
 `
 const SearchBox = styled.div`
-    
+    width: 80%;
+    height: 30px;
+    input{
+        min-width: 500px;
+        width: 100%;
+        border: none;
+        height: 30px;
+        background-color: ${props => props.theme.innerColor};
+        border-radius: 8px;
+        padding: 0 10px 0 10px;
+    }
+    button{
+        border: none;
+        background-color: transparent;
+        height: 25px;
+        svg{
+            width: 20px;
+        }
+    }
 `
 
 function SearchList({category, search, token}:SearchPageProps){
@@ -79,7 +97,11 @@ function SearchList({category, search, token}:SearchPageProps){
             <>
                 <SearchBox>
                     <input type="text" value={keyword} onChange={handleChange}/>
-                    <button onClick={onSearch}>검색</button>
+                    <button onClick={onSearch}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+                        </svg>
+                    </button>
                 </SearchBox>
                 {data?.content.map(search => 
                     <SearchResult 
